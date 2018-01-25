db58b535dd33d9cb724c5103adbabeae66672475
khaled <31278328+khaledprog@users.noreply.github.com>
2018-01-25 23:49:11 +0200

    Create navbar.php

---
+++ admin/includes/templates/navbar.php
@@ -0,0 +1,14 @@
+<div class="w3-bar w3-border w3-black">
+    <a href="dashboard.php" class="w3-bar-item w3-button">Home</a>
+    <a href="users.php?do=Manage" class="w3-bar-item w3-button">Users</a>
+    <a href="subjects.php?do=Manage" class="w3-bar-item w3-button">Subjects</a>
+    <a href="comments.php?do=Manage" class="w3-bar-item w3-button">Comments</a>
+    <div class="w3-dropdown-hover w3-right">
+        <button class="w3-button">Khaled</button>
+        <div class="w3-dropdown-content w3-bar-block w3-card-4">
+            <a href="#" class="w3-bar-item w3-button">Profile</a>
+            <a href="#" class="w3-bar-item w3-button">Settings</a>
+            <a href="logout.php" class="w3-bar-item w3-button">Logout</a>
+        </div>
+    </div>
+</div>
