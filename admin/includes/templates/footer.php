53ab3bd7f9b9d05c94354a646aea8418f33ae2cc
khaled <31278328+khaledprog@users.noreply.github.com>
2018-01-25 23:48:35 +0200

    Create footer.php

---
+++ admin/includes/templates/footer.php
@@ -0,0 +1,8 @@
+    <footer class="w3-container">
+            <h4 class="w3-center w3-teal">Footer</h4>
+        </footer>
+        <!-- JQuery file -->
+        <script src="<?php echo $js?>jquery-3.2.1.slim.min.js"></script>
+        <script src="<?php echo $js?>backend.js"></script>
+    </body>
+</html>
