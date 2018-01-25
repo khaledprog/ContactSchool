031876f72c6b59687832486f2d437aeb6d369603
khaled <31278328+khaledprog@users.noreply.github.com>
2018-01-25 23:48:05 +0200

    Create header.php

---
+++ admin/includes/templates/header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <meta name="description" content="Mini Admin School." />
+        <meta name="keywords" content="school, teacher, student, admin,..." />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <!-- w3.css file -->
+        <link rel="stylesheet" href="<?php echo $css;?>w3.css" />
+        <!-- w3.css food-colors -->
+        <link rel="stylesheet" href="<?php echo $css?>w3-colors-food.css" />
+        <!-- fontawesome file -->
+        <link rel="stylesheet" href="<?php echo $css;?>font-awesome.min.css" />
+        <!-- main css file -->
+        <link rel="stylesheet" href="<?php echo $css;?>backend.css" />
+        <title><?php echo $pageTitle; ?></title>
+    </head>
+    <body>
